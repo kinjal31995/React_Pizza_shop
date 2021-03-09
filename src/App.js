226d809243a0fe01f58402ae44e09b647097cf33
kinjal31995 <kinjal31995@gmail.com>
2021-03-09 22:25:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Pizzeria Express </a>
+      </header>
+      <main>Menu</main>
+      <footer>All Right is reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
