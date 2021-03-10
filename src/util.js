85c4a3b38f3f22f20230888686729d1f781eaceb
kinjal31995 <kinjal31995@gmail.com>
2021-03-11 00:10:50 +0100

    cart-component

--- src/util.js
+++ src/util.js
@@ -1,3 +1,4 @@
 export default function formatCurrency(num) {
-  return "$" + Number(num.toFixed(1)).toLocaleString() + " ";
+  return "€" + Number(num.toFixed(1)).toLocaleString() + " ";
+  // return "\u20AC" + Number(num.toFixed(1)).toLocaleString() + " ";
 }
